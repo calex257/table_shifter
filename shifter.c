02e91530c1e103081b94d1ba69962c0817218070
theallureoftheearth <alxcalciu@gmail.com>
2021-11-17 22:00:49 +0200

    Added input verification and free choice for variable names

--- shifter.c
+++ shifter.c
@@ -12,10 +12,33 @@ void set_nth_bit_on(int* number, int index)
     (*number) = (*number) | (1 << index);
 }
 
-int main(int argc, char* argv[])
+//check if the input is correct
+void verify_input(char* order, char* var_names, int length)
+{
+    int wrong;
+    do {
+        wrong = 0;
+        printf("Enter the new order of the variables\n(without spaces in between)\n");
+        fgets(order, length + 1, stdin);
+        if (strlen(order) != strlen(var_names)) {
+            wrong = 1;
+        }
+        for (int i = 0;i < length && !wrong;i++) {
+            if (!strchr(var_names, order[i])) {
+                wrong = 1;
+            }
+        }
+        if (wrong) {
+            printf("Error. Invalid input\n");
+        }
+        getchar();
+    } while (wrong);
+}
+
+int main(void)
 {
     int var_number, values_number;
-    char* input, * transformed, * order, * var_names, min_var_name;
+    char* input, * transformed, * order, * var_names;
 
     //input is for the initial table state
     //transformed is for the final state which will be outputted
@@ -32,8 +55,7 @@ int main(int argc, char* argv[])
     transformed = (char*)malloc(values_number * sizeof(char));
 
     //receive labels for variables
-    printf("Enter the names of the variables\n(without spaces in between)\n"
-        "permutations of consecutive letters of the alphabet only\n");
+    printf("Enter the names of the variables\n(without spaces in between)\n");
     fgets(var_names, var_number + 1, stdin);
     getchar();
     for (int i = 0;i < values_number;i++) {
@@ -49,23 +71,7 @@ int main(int argc, char* argv[])
     }
 
     //receive user order
-    printf("Enter the new order of the variables\n(without spaces in between)\n");
-    fgets(order, var_number + 1, stdin);
-    //ensure the format is uniform
-    strupr(order);
-
-    //determine smallest value
-    min_var_name = var_names[0];
-    for (int i = 1;i < var_number;i++) {
-        if (var_names[i] < min_var_name) {
-            min_var_name = var_names[i];
-        }
-    }
-
-    //modify order for indices
-    for (int i = 0;i < var_number;i++) {
-        order[i] = var_number - 1 - order[i] + min_var_name;
-    }
+    verify_input(order, var_names, var_number);
 
     //reorder the values and store them in transformed
     for (int i = 0;i < values_number;i++) {
@@ -74,7 +80,13 @@ int main(int argc, char* argv[])
         for (int j = 0;j < var_number;j++) {
             //calculate new index based on previous index
             if (nth_bit_is_on(i, var_number - 1 - j)) {
-                set_nth_bit_on(&index, order[j]);
+                int pos;
+                for (int k = 0;k < var_number;k++) {
+                    if (order[j] == var_names[k]) {
+                        pos = k;
+                    }
+                }
+                set_nth_bit_on(&index, var_number - 1 - pos);
             }
         }
         transformed[i] = input[index];
@@ -83,7 +95,7 @@ int main(int argc, char* argv[])
         for (int j = 0;j < var_number;j++) {
             //displays each line of the table
             //with the corresponding values of the variables
-            printf("%c: %d | ", var_number - 1 - order[j] + min_var_name, nth_bit_is_on(i, var_number - 1 - j));
+            printf("%c: %d | ", order[j], nth_bit_is_on(i, var_number - 1 - j));
         }
         printf("%c\n", transformed[i]);
     }
